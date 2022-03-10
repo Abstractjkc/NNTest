5456a42f8ea7c371869040321c46440fad250dcf
Streamyue <630940124@qq.com>
2022-03-10 14:51:53 +0800

    添加若干注释

--- NNTest/Matrix.h
+++ NNTest/Matrix.h
@@ -2,11 +2,15 @@
 #include <iostream>
 #include <ctime>
 using namespace std;
+/*
+	封装矩阵相关的操作
+	除了矩阵运算外，主要进行内存上的管理与优化
+*/
 class Matrix
 {
-	friend Matrix operator*(const Matrix& a, const Matrix& b);
-	friend ostream& operator<<(ostream& out, const Matrix& mat);
-	friend Matrix operator+(const Matrix& a, const Matrix& b);
+	friend Matrix operator*(const Matrix& a, const Matrix& b);	// 矩阵乘法
+	friend ostream& operator<<(ostream& out, const Matrix& mat);	//输出矩阵
+	friend Matrix operator+(const Matrix& a, const Matrix& b);	// 矩阵加减法
 	friend Matrix operator-(const Matrix& a, const Matrix& b);
 private:
 	double** a;		// 存储二维矩阵
@@ -24,8 +28,8 @@ public :
 	void fill(double num); //将矩阵中所有元素填入一个值
 	void matrixI(); // 构造一个单位矩阵
 	void matrixO(); // 构造一个零矩阵
-	void forEach(double (*f)(double));
-	Matrix mutipleByNumber(double num)const;
-	static double Rand(int l, int r);
+	void forEach(double (*f)(double));	// 对矩阵中每个元素应用函数f
+	Matrix mutipleByNumber(double num)const;	// 矩阵的数乘
+	static double Rand(int l, int r);	// 生成随机数
 };
 
--- NNTest/NNTest.cpp
+++ NNTest/NNTest.cpp
@@ -1,30 +1,42 @@
 ﻿// NNTest.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
-//
-
+// 全连接网络判断坐标点所在的象限
+/**
+    考虑到全连接过程并不复杂，直接选择用面向过程做该实例
+*/
 #include <iostream>
 #include <algorithm>
 #include <cmath>
 #include "Matrix.h"
 using namespace std;
-void getProbs(Matrix &Y);
-double getLoss(const Matrix& probs, const Matrix& label);
-Matrix affine_forward(const Matrix& X, const Matrix& W, const Matrix& b);
+void getProbs(Matrix &Y);       // 获得输出对应标签的概率
+double getLoss(const Matrix& probs, const Matrix& label);   // 获取loss值
+Matrix affine_forward(const Matrix& X, const Matrix& W, const Matrix& b);   // 前向传播函数
+
+// 反向传播函数
 void affine_backward(const Matrix& out, const Matrix &X, const Matrix &W, const Matrix &b, Matrix &dX, Matrix &dW, Matrix &db);
+// relu 激活函数
 double relu(double num) {
     return num <= 0 ? 0 : num;
 }
+
 int main()
 {
+    // ==========数据初始化开始==========
     srand(time(NULL));
     double loss = 0, reg = 0.001, epsilon = 0.001;
     int input_dim = 2, num_classes = 4, hidden_dim = 50, num_test = 4;
+    /*
+        矩阵规模的初始化
+        以及输入数据的初始化
+        容易看到 选了四个点 (1, 1), (-1, 1), (-1, -1), (1, -1) 作为输出数据
+    */
     Matrix input(num_test, input_dim);
     input[0][0] = 1, input[0][1] = 1;
     input[1][0] = -1, input[1][1] = 1;
     input[2][0] = -1, input[2][1] = -1;
     input[3][0] = 1, input[3][1] = -1;
     Matrix label(num_test, 1);
-    label[0][0] = 1, label[1][0] = 2, label[2][0] = 3, label[3][0] = 4;
+    label[0][0] = 1, label[1][0] = 2, label[2][0] = 3, label[3][0] = 4; // 四个点对应的正确标签
     Matrix W1(input_dim, hidden_dim), W2(hidden_dim, num_classes), b1(1, hidden_dim);
     Matrix b2(1, num_classes), H1(num_test, hidden_dim), Relu_Cache(num_test, hidden_dim);
     Matrix dW1(input_dim, hidden_dim), dW2(hidden_dim, num_classes), db1(1, hidden_dim);
@@ -32,15 +44,20 @@ int main()
     Matrix dH(num_test, hidden_dim), dInput(num_test, input_dim);
     W1.randMatrix(), W2.randMatrix();
     b1.fill(0), b2.fill(0);
-    int T = 10000;
+    // ======数据初始化结束=========
+    // =========训练==========
+    int T = 10000;  // 迭代训练次数
     while (T--) {
-        H1 = affine_forward(input, W1, b1);
-        H1.forEach(relu);
+        // 前向传播 激活 前向传播
+        H1 = affine_forward(input, W1, b1); // 第一次前向传播
+        H1.forEach(relu);   //激活
         Relu_Cache = H1;
-        Y = affine_forward(H1, W2, b2);
+        Y = affine_forward(H1, W2, b2); // 第二次前向传播
         probs = Y;
-        getProbs(probs);
-        loss = getLoss(probs, label);
+        getProbs(probs);    // 获得输出对应标签的概率
+        loss = getLoss(probs, label);   // 获得loss值
+
+        //反向传播 基本同上
         for (int i = 0; i < label.row; ++i) {
             probs[i][(int)label[i][0] - 1] -= 1;
         }
@@ -52,6 +69,8 @@ int main()
             }
         }
         affine_backward(dH, input, W1, b1, dInput, dW1, db1);
+
+        // 参数的更新
         dW1 = dW1 + W1.mutipleByNumber(reg);
         dW2 = dW2 + W2.mutipleByNumber(reg);
         W1 = W1 - dW1.mutipleByNumber(epsilon);
@@ -60,6 +79,8 @@ int main()
         b2 = b2 - db2.mutipleByNumber(epsilon);
     }
     cout  << loss / num_test<< "\n";
+
+    // 测试数据
     Matrix test_data(4, 2);
     test_data[0][0] = -1, test_data[0][1] = 2;
     test_data[1][0] = 1, test_data[1][1] = 2;
